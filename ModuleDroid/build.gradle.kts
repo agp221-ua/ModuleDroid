4b6034290695f22574c94db252e4520380eb1549
galaniberico <alexgalanp@gmail.com>
2024-03-09 17:28:44 +0100

    some configuration changes

--- ModuleDroid/build.gradle.kts
+++ ModuleDroid/build.gradle.kts
@@ -37,7 +37,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Remove the deprecated line
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }

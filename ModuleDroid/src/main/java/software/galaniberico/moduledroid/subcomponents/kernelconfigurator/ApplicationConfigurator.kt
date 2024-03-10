3aa7397924f1c3cdb79655d7b7f1f0e679ef998c
galaniberico <alexgalanp@gmail.com>
2024-03-10 18:10:05 +0100

    Release v0.1.1 - added plugin's app configuration support

--- ModuleDroid/src/main/java/software/galaniberico/moduledroid/subcomponents/kernelconfigurator/ApplicationConfigurator.kt
+++ ModuleDroid/src/main/java/software/galaniberico/moduledroid/subcomponents/kernelconfigurator/ApplicationConfigurator.kt
@@ -1,6 +1,12 @@
 package software.galaniberico.moduledroid.subcomponents.kernelconfigurator
 
+import android.app.Application
+import android.content.pm.PackageManager
+import android.health.connect.datatypes.AppInfo
+import android.os.Bundle
+import android.text.TextUtils.split
 import software.galaniberico.moduledroid.subcomponents.activitylifecycle.ActivityLifecycleConfigurator
+import java.io.InvalidClassException
 
 /**
  * This class is responsible of implementing all necessary methods to correctly configure the
@@ -23,6 +29,47 @@ object ApplicationConfigurator {
         subcomponents.forEach {
             it.onApplicationCreate(app)
         }
+        setUpPlugins(app)
+    }
+
+    fun setUpPlugins(app: ModuleDroidApplication) {
+        getConfigurablePlugins(app)
+    }
+
+    private fun getConfigurablePlugins(app: ModuleDroidApplication) {
+        val stringArray = getMetaDataString("moduledroid_plugins", app)
+        for (s in stringArray)
+        try {
+            val pluginClass = Class.forName(s)
+            if (PluginConfigurator::class.java.isAssignableFrom(pluginClass)) {
+                val pluginInstance = pluginClass
+                    .getDeclaredConstructor().newInstance() as PluginConfigurator
+
+                pluginInstance.configure(app)
+            } else {
+                throw UnsupportedOperationException("The class $s does not implement the ModuleDroid interface called 'PluginConfigurator'. Have you written the class path correctly? Maybe it is not the class you should add in 'moduledroid_plugins' section")
+            }
+        } catch (e: ClassNotFoundException) {
+            throw ClassNotFoundException("The class $s can not be found. Have you written the class path correctly? Maybe have you not imported correctly the library?")
+        } catch (e: InstantiationException) {
+            throw InstantiationException("The class $s could not be instantiated. Revise the plugin's library import. If the problem persists, try contacting the plugin author")
+        } catch (e: IllegalAccessException) {
+            throw IllegalAccessException("The class $s could not be accessed. Revise the plugin's library import. If the problem persists, try contacting the plugin author")
+        }
+    }
+
+    private fun getMetaDataString(key: String, app: ModuleDroidApplication): List<String> {
+        return try {
+            val appInfo = app.packageManager
+                .getApplicationInfo(app.packageName, PackageManager.GET_META_DATA)
+            val plugins: List<String>? = appInfo.metaData?.getString(key)?.split(",")
+                ?.map { it.trim().removeSurrounding("\n") }
+
+            plugins ?: emptyList()
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            emptyList()
+        }
     }
 }
 
@@ -35,4 +82,8 @@ interface AppConfigurator {
      * The implementing class must be registered in *ApplicationConfigurator.subcomponents* list.
      */
     fun onApplicationCreate(app: ModuleDroidApplication)
+}
+
+interface PluginConfigurator {
+    fun configure(app: Application)
 }

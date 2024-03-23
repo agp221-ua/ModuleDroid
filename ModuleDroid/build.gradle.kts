234a4a4ad52b6d009130d818d77219b50d3d46b7
galaniberico <alexgalanp@gmail.com>
2024-03-23 20:11:15 +0100

    Release v0.1.2 - adding compatibility with jitpack 4

--- ModuleDroid/build.gradle.kts
+++ ModuleDroid/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    `maven-publish`
+
 }
 
 android {
@@ -34,22 +34,13 @@ android {
     }
 
 }
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "com.github.agp221-ua"
-            artifactId = "ModuleDroid"
-            version = "0.1.2.3"
 
-//            from(components["java"])
-        }
-    }
-}
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("io.github.supertidus:maven-publish:0.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

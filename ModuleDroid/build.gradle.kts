a6deb3123723563efb05fac9a7a23e29e5c7d20a
galaniberico <alexgalanp@gmail.com>
2024-03-29 18:00:00 +0100

    Release v0.1.3.3 Adding string templates for kernel errors

--- ModuleDroid/build.gradle.kts
+++ ModuleDroid/build.gradle.kts
@@ -54,7 +54,7 @@ afterEvaluate {
             from(components.findByName(variant.name))
             groupId = "com.local.agp221-ua"
             artifactId = "ModuleDroid"
-            version = "0.1.3.1"
+            version = "0.1.3.3"
         }
     }
 }

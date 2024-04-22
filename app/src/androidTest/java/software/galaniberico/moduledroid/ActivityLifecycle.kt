868f56c2016f3841abdfc3b568f6246790a2e4ad
galaniberico <alexgalanp@gmail.com>
2024-04-22 23:48:15 +0200

    more tests

---
+++ app/src/androidTest/java/software/galaniberico/moduledroid/ActivityLifecycle.kt
@@ -0,0 +1,35 @@
+package software.galaniberico.moduledroid
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import software.galaniberico.moduledroid.facade.Facade
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ActivityLifecycle {
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun currentActivityOk() {
+        activityRule.scenario.onActivity {
+            assertEquals(it, Facade.getCurrentActivity())
+        }
+    }
+
+
+}

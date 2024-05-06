56d1eeeffb000e155fcf155e74be246283fc0d77
galaniberico <alexgalanp@gmail.com>
2024-05-07 01:22:39 +0200

    all preferences

--- ModuleDroid/src/main/java/software/galaniberico/moduledroid/subcomponents/preferencesmanager/PreferencesManagerConfigurator.kt
+++ ModuleDroid/src/main/java/software/galaniberico/moduledroid/subcomponents/preferencesmanager/PreferencesManagerConfigurator.kt
@@ -1,11 +1,14 @@
 package software.galaniberico.moduledroid.subcomponents.preferencesmanager
 
 import android.content.Context
+import androidx.preference.PreferenceManager
 import software.galaniberico.moduledroid.subcomponents.kernelconfigurator.AppConfigurator
 import software.galaniberico.moduledroid.subcomponents.kernelconfigurator.ModuleDroidApplication
 
+private const val PREFERENCES_FILENAME = "###ModuleDroid_PreferencesManager@PREFERENCE_NAME###"
+
 internal object PreferencesManagerConfigurator : AppConfigurator {
     override fun onApplicationCreate(app: ModuleDroidApplication) {
-        PreferencesManager.preferences = app.getSharedPreferences("###ModuleDroid_PreferencesManager@PREFERENCE_NAME###", Context.MODE_PRIVATE);
+        PreferencesManager.preferences = app.getSharedPreferences(PREFERENCES_FILENAME, Context.MODE_PRIVATE);
     }
 }

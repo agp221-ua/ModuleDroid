95c479e94c5bc3814d036a466717a7c68b29f0af
galaniberico <alexgalanp@gmail.com>
2024-05-19 11:40:01 +0200

    suscription bugs

--- ModuleDroid/src/main/java/software/galaniberico/moduledroid/subcomponents/preferencesmanager/PreferencesManager.kt
+++ ModuleDroid/src/main/java/software/galaniberico/moduledroid/subcomponents/preferencesmanager/PreferencesManager.kt
@@ -1,90 +1,107 @@
 package software.galaniberico.moduledroid.subcomponents.preferencesmanager
 
 import android.content.SharedPreferences
+import java.util.WeakHashMap
 
 internal object PreferencesManager {
     internal var preferences: SharedPreferences? = null
+    internal val secureListenerStore =
+        mutableSetOf<SharedPreferences.OnSharedPreferenceChangeListener>()
 
-    internal fun get(key: String, defaultValue: String) : String{
+    internal fun get(key: String, defaultValue: String): String {
         return preferences?.getString(key, defaultValue) ?: defaultValue
     }
 
-    internal fun get(key: String, defaultValue: Int) : Int{
+    internal fun get(key: String, defaultValue: Int): Int {
         return preferences?.getInt(key, defaultValue) ?: defaultValue
     }
 
-    internal fun get(key: String, defaultValue: Long) : Long{
+    internal fun get(key: String, defaultValue: Long): Long {
         return preferences?.getLong(key, defaultValue) ?: defaultValue
     }
 
-    internal fun get(key: String, defaultValue: Float) : Float{
+    internal fun get(key: String, defaultValue: Float): Float {
         return preferences?.getFloat(key, defaultValue) ?: defaultValue
     }
 
-    internal fun get(key: String, defaultValue: Boolean) : Boolean{
+    internal fun get(key: String, defaultValue: Boolean): Boolean {
         return preferences?.getBoolean(key, defaultValue) ?: defaultValue
     }
 
-    internal fun get(key: String, defaultValue: Set<String>) : Set<String>{
+    internal fun get(key: String, defaultValue: Set<String>): Set<String> {
         return preferences?.getStringSet(key, defaultValue) ?: defaultValue
     }
 
     internal fun set(key: String, value: String) {
-        val editor: SharedPreferences.Editor = preferences?.edit() ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
+        val editor: SharedPreferences.Editor = preferences?.edit()
+            ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
         editor.putString(key, value)
         editor.apply()
     }
 
-    internal fun set(key: String, value: Int){
-        val editor: SharedPreferences.Editor = preferences?.edit() ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
+    internal fun set(key: String, value: Int) {
+        val editor: SharedPreferences.Editor = preferences?.edit()
+            ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
         editor.putInt(key, value)
         editor.apply()
     }
 
-    internal fun set(key: String, value: Long){
-        val editor: SharedPreferences.Editor = preferences?.edit() ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
+    internal fun set(key: String, value: Long) {
+        val editor: SharedPreferences.Editor = preferences?.edit()
+            ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
         editor.putLong(key, value)
         editor.apply()
     }
 
-    internal fun set(key: String, value: Float){
-        val editor: SharedPreferences.Editor = preferences?.edit() ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
+    internal fun set(key: String, value: Float) {
+        val editor: SharedPreferences.Editor = preferences?.edit()
+            ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
         editor.putFloat(key, value)
         editor.apply()
     }
 
-    internal fun set(key: String, value: Boolean){
-        val editor: SharedPreferences.Editor = preferences?.edit() ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
+    internal fun set(key: String, value: Boolean) {
+        val editor: SharedPreferences.Editor = preferences?.edit()
+            ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
         editor.putBoolean(key, value)
         editor.apply()
     }
 
-    internal fun set(key: String, value: Set<String>){
-        val editor: SharedPreferences.Editor = preferences?.edit() ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
+    internal fun set(key: String, value: Set<String>) {
+        val editor: SharedPreferences.Editor = preferences?.edit()
+            ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
         editor.putStringSet(key, value)
         editor.apply()
     }
 
-    internal fun clear(){
-        val editor: SharedPreferences.Editor = preferences?.edit() ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
+    internal fun clear() {
+        val editor: SharedPreferences.Editor = preferences?.edit()
+            ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
         editor.clear()
         editor.apply()
     }
 
-    internal fun clearForce(){ //Remove all keys
-        val editor: SharedPreferences.Editor = preferences?.edit() ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
-        for (k in preferences?.all?.keys ?: emptySet<String>()){
+    internal fun clearForce() { //Remove all keys
+        val editor: SharedPreferences.Editor = preferences?.edit()
+            ?: throw IllegalStateException("Attempting to put values into null preferences. Maybe the plugin is not correctly configured?")
+        for (k in preferences?.all?.keys ?: emptySet<String>()) {
             editor.remove(k)
         }
         editor.apply()
     }
 
-    internal fun addSubscription(lambda:(SharedPreferences, String?) -> Unit){
-        preferences?.registerOnSharedPreferenceChangeListener(lambda)
+    internal fun addSubscription(lambda: (SharedPreferences, String?) -> Unit) {
+        SharedPreferences.OnSharedPreferenceChangeListener(lambda).let {
+            secureListenerStore.add(it)
+            preferences?.registerOnSharedPreferenceChangeListener(it)
+        }
     }
 
-    internal fun removeSubscription(lambda:(SharedPreferences, String?) -> Unit){
-        preferences?.unregisterOnSharedPreferenceChangeListener(lambda)
+    internal fun removeSubscription(lambda: (SharedPreferences, String?) -> Unit) {
+        SharedPreferences.OnSharedPreferenceChangeListener(lambda).let {
+            secureListenerStore.remove(it)
+            preferences?.unregisterOnSharedPreferenceChangeListener(it)
+        }
     }
 
 
